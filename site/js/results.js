16674ef705810a4e8a9744f12a00cd1a9b805069
Master_Grid <Master_Grid@hotmail.com>
2016-11-14 16:41:18 +0000

    Replaced MySQL database with FireBase database;

--- site/js/results.js
+++ site/js/results.js
@@ -1,27 +1,87 @@
-/* global $*/
+/* global $ firebase*/
 
 var del = 300;
+var database = firebase.database();
 //var dataset = null;
 
 var getData = function getData(dset,callback) {
-    $.ajax({
-        type: "GET",
-        url: 'https://ddp3-cloned-mastergrid.c9users.io/site/php/get'+ dset +'.php',
-        data: '',
-        dataType: "html",
-        success: function(data) {
-            //alert(data);
-            var array = JSON.parse(data);
-            callback(array);
-        },
-        error: function() {
-            alert('Error occured. Zorg ervoor dat je webadres met https:// begint!');
-        }
-    });
+    if (dset == 1) {
+        var countTest = 0;
+        var countSwitch = 0;
+        var countSwitchWin = 0;
+        var countStickWin = 0;
+        database.ref('results').once('value', function(snapshot) {
+            snapshot.forEach(function(child) {
+                var perform = child.val();
+                if (perform.why != -1) {
+                    countTest = countTest + 1;
+                    if (perform.switch == 1) {
+                        countSwitch++;
+                        if (perform.win == 1) {
+                            countSwitchWin++;
+                        }
+                    } else {
+                        if (perform.win == 1) {
+                            countStickWin++;
+                        }
+                    }
+                }
+            });
+            callback([countTest, countSwitch, countSwitchWin, countStickWin]);
+        });
+    } else if (dset == 2) {
+        var vwoTests, havoTests, vmboTests, vwo1Tests, vwo2Tests, vwo3Tests, vwo4Tests, vwo5Tests, vwo6Tests, havo1Tests, havo2Tests, havo3Tests, havo4Tests, havo5Tests, vmbo1Tests, vmbo2Tests, vmbo3Tests, vmbo4Tests;
+        vwoTests = vwo1Tests = vwo2Tests = vwo3Tests = vwo4Tests = vwo5Tests = vwo6Tests = havoTests = havo1Tests = havo2Tests = havo3Tests = havo4Tests = havo5Tests = vmboTests = vmbo1Tests = vmbo2Tests = vmbo3Tests = vmbo4Tests = 0;
+        database.ref('results').once('value', function(snapshot) {
+            snapshot.forEach(function(child) {
+                var perform = child.val();
+                if (perform.why != -1) {
+                    if (perform.niveau == 1) {
+                        vmboTests++;
+                        eval("vmbo" + perform.jaar + "Tests++;");
+                    } else if (perform.niveau == 2) {
+                        havoTests++;
+                        eval("havo" + perform.jaar + "Tests++;");
+                    } else if (perform.niveau == 3) {
+                        vwoTests++;
+                        eval("vwo" + perform.jaar + "Tests++;");
+                    }
+                }
+            });
+            callback([vwoTests, havoTests, vmboTests, vwo1Tests, vwo2Tests, vwo3Tests, vwo4Tests, vwo5Tests, vwo6Tests, havo1Tests, havo2Tests, havo3Tests, havo4Tests, havo5Tests, vmbo1Tests, vmbo2Tests, vmbo3Tests, vmbo4Tests]);
+        });
+    } else if (dset == 3) {
+        var numSwitch, numStick, numSwitch1, numSwitch2, numSwitch3, numSwitch4, numStick1, numStick2, numStick3, numStick4, switchWhy, stickWhy;
+        numSwitch = numStick = numSwitch1 = numSwitch2 = numSwitch3 = numSwitch4 = numStick1 = numStick2 = numStick3 = numStick4 = 0;
+        switchWhy = [];
+        stickWhy = [];
+        database.ref('results').once('value', function(snapshot) {
+            snapshot.forEach(function(child) {
+                var perform = child.val();
+                if (perform.why != -1) {
+                    if (perform.switch == 1) {
+                        numSwitch++;
+                        eval("numSwitch" + perform.why + "++;");
+                        if (perform.why == 4) {
+                            switchWhy.unshift(perform.why2);
+                        }
+                    } else if (perform.switch == 0) {
+                        numStick++;
+                        eval("numStick" + perform.why + "++;");
+                        if (perform.why == 4) {
+                            stickWhy.unshift(perform.why2);
+                        }
+                    }
+                }
+            });
+            callback([numSwitch, numStick, numSwitch1, numSwitch2, numSwitch3, numSwitch4, numStick1, numStick2, numStick3, numStick4, switchWhy, stickWhy]);
+        });
+    }
 };
 
 var refresh = function refresh() {
     var step = 0;
+    $(".why2").children().remove();
     getData(1, function(inp) {
         $("#numTests").html(inp[0]);
         $("#numSwitch").html(inp[1] + " (" + Math.floor(inp[1] / inp[0] * 100) + "% van testen)");
@@ -46,26 +106,24 @@ var refresh = function refresh() {
         step = step + 1;
     });
     getData(3, function(inp) {
-        var inp1 = inp[0];
-        $(".switchers").text(inp1[0]);
-        $(".stickers").text(inp1[1]);
+        //var inp1 = inp[0];
+        $(".switchers").text(inp[0]);
+        $(".stickers").text(inp[1]);
         for (var i = 2; i <= 5; i++) {
-            $(".whys" + i).css("width",Math.floor(inp1[i] / inp1[0] * 100) + "%").text("("+inp1[i]+")");
+            $(".whys" + i).css("width",Math.floor(inp[i] / inp[0] * 100) + "%").text("("+inp[i]+")");
         }
         for (var i = 6; i <= 9; i++) {
-            $(".whys" + i).css("width",Math.floor(inp1[i] / inp1[1] * 100) + "%").text("("+inp1[i]+")");
+            $(".whys" + i).css("width",Math.floor(inp[i] / inp[1] * 100) + "%").text("("+inp[i]+")");
         }
-        var inp2 = inp[1];
-        for (var i = 0; i <= inp2.length-1; i++) {
-            $(".whySwitch").prepend("<p>" + inp2[i] + "<p>" + "<hr>");
+        for (var i = 0; i < inp[10].length; i++) {
+            $(".whySwitch").prepend("<p>" + inp[10][i] + "<p>" + "<hr>");
         }
-        var inp3 = inp[2];
-        for (var i = 0; i <= inp3.length-1; i++) {
-            $(".whyStick").prepend("<p>" + inp3[i] + "<p>" + "<hr>");
+        for (var i = 0; i < inp[11].length; i++) {
+            $(".whyStick").prepend("<p>" + inp[11][i] + "<p>" + "<hr>");
         }
         $(".whySwitch").children("hr:last-child").hide();
         $(".whyStick").children("hr:last-child").hide();
-        step = step + 1
+        step = step + 1;
     });
     $(".loadDone").slideDown(del);
 };
@@ -129,11 +187,11 @@ var jinx = function jinx() {
     $(".loadDone").children("a").click(function() {
         $(".loadDone").slideUp(del);
     });
-    $.keyDown(function() {
+    /*$.keyDown(function() {
         if (event.which == 17) {
             $.keyUp()
         }
-    })
+    })*/
 };
 
 $(document).ready(function() {
